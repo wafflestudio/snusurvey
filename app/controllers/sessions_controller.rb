29814642fb5173ee3967e6b7a4f98fd24394fd9c
Taekmin Kim <tantara.tm@gmail.com>
2012-10-07 21:37:10 +0900

    temp

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -43,9 +43,9 @@ class SessionsController < ApplicationController
 
   private
   def get_key
-    @app_id = "340384302721771"
-    @secret = "923542491f5bdbe13fddc497c73dc457"
-    @callback = "http://localhost:4000/signin/facebook"
+    @app_id = "111159975701312"
+    @secret = "4444af97c4577a0cfc7d770b0b5f3072"
+    @callback = "http://dev.wafflestudio.net:4011/signin/facebook"
     @oauth = Koala::Facebook::OAuth.new(@app_id, @secret, @callback)
   end
 end
