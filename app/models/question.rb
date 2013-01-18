42224352f038fedc221eabc1686ab5ea97650d7e
Arif Jafer <majafer10@gmail.com>
2013-01-18 10:42:25 +0900

    Few Bug Fixes in the Models

--- app/models/question.rb
+++ app/models/question.rb
@@ -9,7 +9,7 @@ class Question
   belongs_to :question_template, :class_name => "QuestionTemplate", :inverse_of => :questions  # 템플릿
   belongs_to :survey                                                                  # 설문
   has_many :replies, :dependent => :destroy                                           # 응답
-  has_many :pictures, :as => :picturable, :dependent => :destroy                                          # 사진
+  has_many :pictures, :as => :picturable, :dependent => :destroy                      # 사진
   has_many :examples, :dependent => :destroy
 
 # validations
